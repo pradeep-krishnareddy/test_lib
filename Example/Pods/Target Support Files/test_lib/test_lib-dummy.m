f0fbf24cc10c325cf735dc95f0f91c2fce299f36
pradeep-krishnareddy <pradeep4411@gmail.com>
2016-12-07 12:08:28 +0530

    test commit

---
+++ Example/Pods/Target Support Files/test_lib/test_lib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_test_lib : NSObject
+@end
+@implementation PodsDummy_test_lib
+@end
---
+++ Example/Pods/Target Support Files/test_lib/test_lib-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double test_libVersionNumber;
+FOUNDATION_EXPORT const unsigned char test_libVersionString[];
+
